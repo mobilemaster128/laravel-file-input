9c30fe3d24916d582d4b593f2a51851d436e382a
DESKTOP-L827G08\Developer <mobilemaster128@gmail.com>
2018-01-21 12:59:31 -0800

    no message

--- src/Receiver.php
+++ src/Receiver.php
@@ -40,11 +40,11 @@ class Receiver
     private function appendData($filePathPartial, UploadedFile $file)
     {
         if (!$out = @fopen($filePathPartial, 'ab')) {
-            throw new PluploadException('Failed to open output stream.', 102);
+            throw new FileInputException('Failed to open output stream.', 102);
         }
 
         if (!$in = @fopen($file->getPathname(), 'rb')) {
-            throw new PluploadException('Failed to open input stream', 101);
+            throw new FileInputException('Failed to open input stream', 101);
         }
         while ($buff = fread($in, 4096)) {
             fwrite($out, $buff);
@@ -54,33 +54,6 @@ class Receiver
         @fclose($in);
     }
 
-    public function receiveChunks($name, Closure $handler)
-    {
-        $result = false;
-
-        if ($this->request->file($name)) {
-            $file = $this->request->file($name);
-            $chunk = (int) $this->request->get('chunk', false);
-            $chunks = (int) $this->request->get('chunks', false);
-            $originalName = $this->request->get('name');
-
-            $filePath = $this->getPath().'/'.$originalName.'.part';
-            $this->removeOldData($filePath);
-
-            $this->appendData($filePath, $file);
-
-            if ($chunk == $chunks - 1) {
-                $file = new UploadedFile($filePath, $originalName, 'blob', sizeof($filePath), UPLOAD_ERR_OK, true);
-
-                $result = $handler($file);
-
-                @unlink($filePath);
-            }
-        }
-
-        return $result;
-    }
-
     public function removeOldData($filePath)
     {
         if (file_exists($filePath) && filemtime($filePath) < time() - $this->maxFileAge) {
@@ -88,21 +61,12 @@ class Receiver
         }
     }
 
-    public function hasChunks()
-    {
-        return (bool) $this->request->get('chunks', false);
-    }
-
     public function receive($name, Closure $handler)
     {
         $response = [];
         $response['jsonrpc'] = '2.0';
 
-        if ($this->hasChunks()) {
-            $result = $this->receiveChunks($name, $handler);
-        } else {
-            $result = $this->receiveSingle($name, $handler);
-        }
+        $result = $this->receiveSingle($name, $handler);
 
         $response['result'] = $result;
 
