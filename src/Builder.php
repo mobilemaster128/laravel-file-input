e1708abdb0669975e856771522bdddfbd78f735d
DESKTOP-L827G08\Developer <mobilemaster128@gmail.com>
2018-01-28 08:43:18 -0800

    multi language

--- src/Builder.php
+++ src/Builder.php
@@ -27,15 +27,17 @@ class Builder
     {
         $scripts = <<<EOC
         <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-        <link href="/vendor/mobilemaster/file-input/css/fileinput.min.css" media="all" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('vendor/mobilemaster/file-input/css/fileinput.min.css') }}" media="all" rel="stylesheet" type="text/css" />
         <script src="//code.jquery.com/jquery-3.2.1.min.js"></script>
-        <script src="/vendor/mobilemaster/file-input/js/plugins/piexif.min.js" type="text/javascript"></script>
-        <script src="/vendor/mobilemaster/file-input/js/plugins/sortable.min.js" type="text/javascript"></script>
-        <script src="/vendor/mobilemaster/file-input/js/plugins/purify.min.js" type="text/javascript"></script>
-        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <script src="/vendor/mobilemaster/file-input/js/fileinput.min.js"></script>
-        <script src="/vendor/mobilemaster/file-input/themes/fa/theme.js"></script>
-        <script src="/vendor/mobilemaster/file-input/js/locales/(lang).js"></script>
+        <script src="{{ asset('vendor/mobilemaster/file-input/js/plugins/piexif.min.js') }}" type="text/javascript"></script>
+        <script src={{ asset('vendor/mobilemaster/file-input/js/plugins/sortable.min.js') }}" type="text/javascript"></script>
+        <script src={{ asset('vendor/mobilemaster/file-input/js/plugins/purify.min.js') }}" type="text/javascript"></script>
+        <script src={{ asset('/maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js') }}"></script>
+        <script src={{ asset('vendor/mobilemaster/file-input/js/fileinput.min.js') }}"></script>
+        <script src={{ asset('vendor/mobilemaster/file-input/themes/fa/theme.js') }}"></script>
+        @isset(config('fileinput.lang'))
+        <script src={{ asset('vendor/mobilemaster/file-input/js/locales/' . config('fileinput.lang'). '.js') }}"></script>
+        @endisset
 EOC;
         return $scripts;
     }
